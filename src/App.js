42c2fba9ef4ff340c8203f5be62a861d46dd3447
= <=>
2023-01-03 20:45:04 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import BasicForm from "./components/BasicForm";
+import AdvancedForm from "./components/AdvancedForm";
+import "./App.css";
 
 function App() {
+  const [view, setView] = useState("basic");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav>
+        <h3
+          onClick={() => setView("basic")}
+          style={{ color: view === "basic" ? "#fff" : "" }}
         >
-          Learn React
-        </a>
-      </header>
+          Basic
+        </h3>
+        <h3
+          onClick={() => setView("advanced")}
+          style={{ color: view === "advanced" ? "#fff" : "" }}
+        >
+          Advanced
+        </h3>
+      </nav>
+      {view === "basic" ? <BasicForm /> : <AdvancedForm />}
     </div>
   );
 }
