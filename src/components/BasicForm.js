391fd8664fda9fc53e92a2e50e0305eef2a1aaac
= <=>
2023-01-03 21:01:52 +0530

    basic schema created

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,8 +1,56 @@
+import { useFormik } from "formik";
+
 const BasicForm = () => {
+  const { values, handleBlur, handleChange } = useFormik({
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
   return (
     <form autoComplete="off">
       <label htmlFor="email">Email</label>
-      <input id="email" type="email" placeholder="Enter your email" />
+      <input
+        value={values.email}
+        onChange={handleChange}
+        id="email"
+        type="email"
+        placeholder="Enter your email"
+      />
+
+      <label htmlFor="email">age</label>
+      <input
+        value={values.age}
+        onChange={handleChange}
+        id="age"
+        type="number"
+        placeholder="Enter your age"
+        onBlur={handleBlur}
+      />
+
+      <label htmlFor="email">Password</label>
+      <input
+        value={values.Password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="Password"
+        type="Password"
+        placeholder="Enter your Password"
+      />
+
+      <label htmlFor="email">Confirm Password</label>
+      <input
+        value={values.confirmPassword}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        id="Password"
+        type="Password"
+        placeholder="confirm your Password"
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 };
