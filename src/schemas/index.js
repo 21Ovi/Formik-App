913d7e7e91f07cb3813dc3552384c93440c54054
= <=>
2023-01-03 21:02:36 +0530

    basic schema created

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 // min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
 
-const basicSchema = yup.object().shape({
+export const basicSchema = yup.object().shape({
   email: yup.string().email("Please enter a valid email").required("Required"),
   age: yup.number().positive().integer().required("Required"),
   password: yup
